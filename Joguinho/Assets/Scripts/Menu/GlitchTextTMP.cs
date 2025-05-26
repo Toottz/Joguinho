775affbd91fbf54e5ace560b24b32cd4d8c90bf5
Toottz <144941158+Toottz@users.noreply.github.com>
2025-05-26 12:10:34 -0300

    build 0.7

---
+++ Joguinho/Assets/Scripts/Menu/GlitchTextTMP.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class GlitchTextTMP : MonoBehaviour
+{
+    public TextMeshProUGUI uiText;
+    public float glitchInterval = 3f;
+    public float glitchDuration = 0.2f;
+    public string[] glitchChars = new string[] { "#", "%", "&", "$", "@", "!", "*", "?", "/", "+", "█" };
+
+    private string originalText;
+
+    void Start()
+    {
+        if (uiText == null)
+            uiText = GetComponent<TextMeshProUGUI>();
+
+        originalText = uiText.text;
+        StartCoroutine(GlitchLoop());
+    }
+
+    IEnumerator GlitchLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(glitchInterval - 1f, glitchInterval + 1f));
+            StartCoroutine(DoGlitch());
+        }
+    }
+
+    IEnumerator DoGlitch()
+    {
+        string glitchedText = "";
+        for (int i = 0; i < originalText.Length; i++)
+        {
+            if (Random.value > 0.7f)
+                glitchedText += glitchChars[Random.Range(0, glitchChars.Length)];
+            else
+                glitchedText += originalText[i];
+        }
+
+        uiText.text = glitchedText;
+        yield return new WaitForSeconds(glitchDuration);
+        uiText.text = originalText;
+    }
+}
---
+++ Joguinho/Assets/Scripts/Menu/GlitchTextVisualTMP.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class GlitchTextVisualTMP : MonoBehaviour
+{
+    public TextMeshProUGUI uiText;
+    public float glitchInterval = 2f;
+    public float glitchDuration = 0.15f;
+    public float positionOffset = 20f;
+
+    private string originalText;
+    private Vector3 originalPosition;
+    private Color originalColor;
+
+    private Color[] glitchColors = new Color[]
+    {
+        Color.red,
+        Color.green,
+        Color.blue,
+        Color.cyan,
+        Color.magenta,
+        Color.yellow
+    };
+
+    void Start()
+    {
+        if (uiText == null)
+            uiText = GetComponent<TextMeshProUGUI>();
+
+        originalText = uiText.text;
+        originalPosition = uiText.rectTransform.anchoredPosition;
+        originalColor = uiText.color;
+
+        StartCoroutine(GlitchLoop());
+    }
+
+    IEnumerator GlitchLoop()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(glitchInterval - 0.5f, glitchInterval + 1f));
+            StartCoroutine(DoGlitch());
+        }
+    }
+
+    IEnumerator DoGlitch()
+    {
+        Vector2 offset = new Vector2(
+            Random.Range(-positionOffset, positionOffset),
+            Random.Range(-positionOffset, positionOffset)
+        );
+
+        uiText.rectTransform.anchoredPosition = originalPosition + (Vector3)offset;
+        uiText.color = glitchColors[Random.Range(0, glitchColors.Length)];
+
+        yield return new WaitForSeconds(glitchDuration);
+
+        uiText.rectTransform.anchoredPosition = originalPosition;
+        uiText.color = originalColor;
+    }
+}
