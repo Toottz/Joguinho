aca76fbe4a853fff083033b6923af3958b55dd58
Byleth3D <majufr1234@gmail.com>
2025-05-30 14:59:41 -0300

    Mudancas Dia 30

--- Joguinho/Assets/Scripts/TimeManager.cs
+++ Joguinho/Assets/Scripts/TimeManager.cs
@@ -8,6 +8,24 @@ public class TimeManager : MonoBehaviour
     public Light directionalLight; // luz do sol
     public Gradient corLuzPorHora; // curva de cor ao longo do dia
 
+    public Material SkyBoxMaterial;
+    public Gradient corSkybox1;
+    public Gradient corSkybox2;
+    public Gradient corNuvens;
+
+    public Material Casa1;
+    public Material Casa2;
+    public Material Casa3; 
+    public Material Casa4;
+    public Material Casa5;
+    public Material Casa6;
+
+    public Gradient GradienteCasa1;
+    public Gradient GradienteCasa2;
+    public Gradient GradienteCasa3;
+    public Gradient GradienteCasa4;
+    public Gradient GradienteCasa5;
+
     public bool usarMudancaDeCor = true;
 
     [SerializeField]
@@ -60,7 +78,19 @@ public class TimeManager : MonoBehaviour
         if (directionalLight != null && corLuzPorHora != null)
         {
             directionalLight.color = corLuzPorHora.Evaluate(t);
-            directionalLight.transform.rotation = Quaternion.Euler(new Vector3((t * 360f) - 90f, 170f, 0));
+            directionalLight.transform.rotation = Quaternion.Euler(new Vector3((t * 360f) - 90f, 105.104f, 0));
+        }
+        if (SkyBoxMaterial != null && corLuzPorHora != null)
+        {
+
+            SkyBoxMaterial.SetColor("_Cor_Horizonte", corSkybox1.Evaluate(t));
+            SkyBoxMaterial.SetColor("_Cor_ceu", corSkybox2.Evaluate(t));
+            SkyBoxMaterial.SetColor("_Sky_Noise_Color", corNuvens.Evaluate(t));
+            Casa1.SetColor("_EmissionColor", GradienteCasa1.Evaluate(t));
+            Casa2.SetColor("_EmissionColor", GradienteCasa2.Evaluate(t));
+            Casa3.SetColor("_EmissionColor", GradienteCasa3.Evaluate(t));
+            Casa4.SetColor("_EmissionColor", GradienteCasa4.Evaluate(t));
+            Casa5.SetColor("_EmissionColor", GradienteCasa5.Evaluate(t));
         }
     }
 
